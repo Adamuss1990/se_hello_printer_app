aa6b6b3e93f9b6cdaeac93b0c2755b641b32283c
Adamuss1990 <Adamuss7@vp.pl>
2019-02-10 14:23:50 +0100

    xml

--- test/test_views.py
+++ test/test_views.py
@@ -15,3 +15,8 @@ class FlaskrTestCase(unittest.TestCase):
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
         self.assertEquals('{ "imie":"Adam", "mgs":Witaj Swiecie!"}', rv.data)
+
+    def test_msg_with_output_xml(self):
+        rv = self.app.get('/?output=xml')
+        self.assertEquals('<greetings> <name>' "Adam" '</name> <msg>'
+                            "Witaj Swiecie!" '</msg> <greetings>', rv.data)
