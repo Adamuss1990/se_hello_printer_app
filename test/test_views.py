ed51d6a6733cf1a11b0044a03a971eff88e06d2d
Adamuss1990 <Adamuss7@vp.pl>
2019-02-10 13:07:55 +0100

    cos_sie_popsulo_3

--- test/test_views.py
+++ test/test_views.py
@@ -19,4 +19,5 @@ class FlaskrTestCase(unittest.TestCase):
     def test_msg_with_given_name(self):
         imie_test = 'Maria'
         rv = self.app.get('/?output=json&imie=' + imie_test)
-        self.assertEquals('{ "imie":"' + imie_test + '","msg":"Witaj Swiecie!"}', rv.data)
+        self.assertEquals('{ "imie":"' + imie_test + '","msg":"Witaj Swiecie!"}',
+                                        rv.data)
